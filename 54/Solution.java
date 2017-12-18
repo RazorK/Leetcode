7a9e481c15431ede557593fa11c8004b1e69c602
Razor King <aimin52qinhao@163.com>
2017-12-18 14:49:39 -0500

    add 54

---
+++ 54/Solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        if(matrix.length == 0) return result;
+        int rowS = 0, rowE =matrix[0].length-1, colS = 0, colE = matrix.length-1;
+
+        // BUG
+        //  remember to process the situation rowS==rowE and colS == rowE;
+        while(rowS<=rowE && colS<=colE) {
+            for(int i=rowS; i<rowE; i++) result.add(matrix[colS][i]);
+            for(int i=colS; i<colE; i++) result.add(matrix[i][rowE]);
+            if(colS!=colE) {
+                for(int i=rowE; i>rowS; i--) result.add(matrix[colE][i]);
+            } else {
+                result.add(matrix[colS][rowE]);
+            }
+            if(rowS!=rowE){
+                for(int i=colE; i>colS; i--) result.add(matrix[i][rowS]);
+            } else {
+                result.add(matrix[colE][rowS]);
+            }
+            if(colS==colE && rowS==rowE) {
+                result.remove(result.size()-1);
+            }
+            rowS++; rowE--; colS++; colE--;
+        }
+        return result;
+    }
+}
