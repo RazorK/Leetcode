dcc555750a3bc9a6272e8c9b25d5faa11f6371ed
Raz <aimin52qinhao@163.com>
2017-12-21 23:07:42 -0500

    temp

---
+++ 49/Main.java
@@ -0,0 +1,15 @@
+import static java.lang.System.*;
+import java.util.Arrays;
+import java.util.*;
+public class Main {
+    public static void main(String [] args) {
+        String [] inputs = new String [] {
+            "hos","boo","nay","deb","wow","bop","bob","brr","hey","rye","eve",
+            "elf","pup","bum","iva","lyx","yap","ugh","hem","rod","aha","nam",
+            "gap","yea","doc","pen","job","dis","max","oho","jed","lye","ram",
+            "pup","qua","ugh","mir","nap","deb","hog","let","gym","bye","lon",
+            "aft","eel","sol","jab"
+        };
+        System.out.println(Solution.groupAnagrams(inputs));
+	}
+}
---
+++ 49/Solution.java
@@ -0,0 +1,118 @@
+// Given an array of strings, group anagrams together.
+//
+// For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"],
+// Return:
+//
+// [
+//   ["ate", "eat","tea"],
+//   ["nat","tan"],
+//   ["bat"]
+// ]
+// Note: All inputs will be in lower-case.
+import java.util.*;
+class Solution {
+
+    // First idea, use hashmap to recognize whether a word is anagram or a new one.
+
+    // BUG 1, can not only use the char-> bool map, the string may contains same char for
+    // several times.
+
+    // time exceed..
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        ArrayList<HashMap<Character, Integer>> mapList = new ArrayList<>();
+        List<List<String>> result = new ArrayList<>();
+        for(int i = 0; i< strs.length; i++) {
+            boolean found =false;
+            for(int j=0; j<mapList.size(); j++) {
+                if(match(mapList.get(j), strs[i])){
+                    result.get(j).add(strs[i]);
+                    found = true;
+                    break;
+                }
+            }
+            if(!found) {
+                mapList.add(newMap(strs[i]));
+                List<String> temp = new ArrayList<>();
+                temp.add(strs[i]);
+                result.add(temp);
+            }
+        }
+        return result;
+    }
+
+    public static boolean match(HashMap<Character, Integer> map, String str) {
+        HashMap<Character, Integer> clone = (HashMap<Character, Integer>)map.clone();
+        for(int i=0; i< str.length(); i++) {
+            char temp = str.charAt(i);
+            if(!clone.containsKey(temp)) return false;
+            int now = clone.get(temp);
+            if(--now == 0) {
+                clone.remove(temp);
+            } else {
+                clone.put(temp, now);
+            }
+        }
+        if(clone.isEmpty()) return true;
+        return false;
+    }
+
+    public static HashMap<Character, Integer>  newMap(String str) {
+        HashMap<Character, Integer> result = new HashMap<>();
+        for(int i=0; i< str.length(); i++) {
+            char temp = str.charAt(i);
+            if(!result.containsKey(temp)) {
+                result.put(temp, 1);
+            } else {
+                result.put(temp, result.get(temp)+1);
+            }
+        }
+        return result;
+    }
+
+    // NOTE improve ideas: the input are limited in 26 characters.
+    // Firstly count, for each string, change it into a counted ArrayList
+    // I think this shoule be similiar with hashmap, try whether this work.
+
+    // still exceed. gj leetcode~
+    public static boolean strequ(int [] str1, int [] str2) {
+        if(str1.length != str2.length) return false;
+        for(int i=0; i<str1.length; i++) {
+            if(str1[i]!=str2[i]) return false;
+        }
+        return true;
+    }
+
+    public static int[] getList(String str) {
+        int [] result = new int[26];
+        for(int i=0; i<str.length();i++) {
+            result[(str.charAt(i)-'a')]++;
+        }
+        return result;
+    }
+
+    public static List<List<String>> improve(String[] strs) {
+        List<List<String>> result = new ArrayList<>();
+        int l = strs.length;
+        int row = 0;
+        int [][] map = new int [l][26];
+
+        for(int i=0; i<strs.length; i++) {
+            boolean found = false;
+            for(int j=0; j<row; j++) {
+                if(strequ(getList(strs[i]), map[j])) {
+                    result.get(j).add(strs[i]);
+                    found = true;
+                    break;
+                }
+            }
+            if(!found) {
+                List<String> temp = new ArrayList<>();
+                temp.add(strs[i]);
+                result.add(temp);
+                map[row++] = getList(strs[i]);
+            }
+        }
+        return result;
+    }
+
+}
