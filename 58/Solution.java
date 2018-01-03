fe232fe041b49c537636e60214f45aaae73bb4d0
Razor King <aimin52qinhao@163.com>
2018-01-02 20:16:10 -0500

    58

---
+++ 58/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    // Given a string s consists of upper/lower-case alphabets and empty space characters ' ',
+    // return the length of last word in the string.
+    //
+    // If the last word does not exist, return 0.
+    //
+    // Note: A word is defined as a character sequence consists of non-space characters only.
+    //
+    // Example:
+    //
+    // Input: "Hello World"
+    // Output: 5
+    public int lengthOfLastWord(String s) {
+        s = s.trim();
+        if(s.equals("")) return 0;
+        int count = 0;
+        int ptr = s.length() - 1;
+        // BUG: ptr>=0
+        while(ptr>=0 && s.charAt(ptr--)!=' ') count++;
+        return count;
+    }
+}
