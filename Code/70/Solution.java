16578fa7827cc64cd6ef2c8f247f254241b04462
Razor King <aimin52qinhao@163.com>
2018-01-05 12:40:05 -0500

    70

--- Code/70/Solution.java
+++ Code/70/Solution.java
@@ -24,7 +24,40 @@ class Solution {
     // 1. 1 step + 1 step + 1 step
     // 2. 1 step + 2 steps
     // 3. 2 steps + 1 step
-    public int climbStairs(int n) {
 
+    // DP may solve this, just try.
+    public int climbStairs(int n) {
+        if(n<=3) return n;
+        int [] board = new int[n];
+        board[0] = 1;
+        board[1] = 2;
+        for(int i=2; i<n; i++) {
+            board[i] = board[i-1] + board[i-2];
+        }
+        return board[n-1];
+    }
+
+    // still we can simplify the dp solution.
+    // we don't really need such a big board
+    // didn't make result any quicker
+    // maybe because the faster results are just eariler version of problem.
+    // but why keep these results.
+    public int simplifyDP(int n) {
+        if(n<=3) return n;
+        int pre1 = 1, pre2 = 2;
+        int result = 0;
+        for(int i = 2; i<n; i++) {
+            result = pre1 + pre2;
+            pre1 = pre2;
+            pre2 = result;
+        }
+        return result;
+    }
+
+    // still the problem can be solved by dfs
+    // time limit exceed...
+    public int dfs(int n) {
+        if(n<=3) return n;
+        return dfs(n-2) + dfs(n-1);
     }
 }
