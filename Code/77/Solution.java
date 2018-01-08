fa7340f38c394c114cead67bc0d045c766ef16ab
Razor King <aimin52qinhao@163.com>
2018-01-07 21:09:50 -0500

    add 77

---
+++ Code/77/Solution.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class Solution {
+    // Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+    //
+    // For example,
+    // If n = 4 and k = 2, a solution is:
+    //
+    // [
+    //   [2,4],
+    //   [3,4],
+    //   [2,3],
+    //   [1,2],
+    //   [1,3],
+    //   [1,4],
+    // ]
+
+    // first try dfs.
+    public static List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+        if( n<k || n==0 || k==0 ) return result;
+        List<Integer> current = new ArrayList<>();
+        dfs(result, current, n, k);
+        return result;
+    }
+
+    public static void dfs(List<List<Integer>> result, List<Integer> current, int n, int k) {
+        if(k == 0) {
+            // BUG cannot directly add current here, because the current will be added as reference
+            // and the current will change later
+            List<Integer> copy = (List<Integer>)((ArrayList<Integer>)current).clone();
+            result.add(copy);
+        }
+        for(int i=n; i>=1; i--) {
+            current.add(i);
+            dfs(result, current, i-1, k-1);
+            current.remove(current.size()-1);
+        }
+    }
+}
--- Idea/GreatTricks.md
+++ Idea/GreatTricks.md
@@ -3,3 +3,5 @@
     + use `left + (right - left) / 2`
     + rather than `(left + right) / 2`
     + because `(left + right)` may exceed the boundary.
+- Sometimes, if the input are bounded in a known range, we can use array in place of HashMap.
+    + eg. 76.
