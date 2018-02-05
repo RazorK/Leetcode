c2ebd1ed92e96ecf6860fc7b130c73e2ca5ddc87
Razor King <aimin52qinhao@163.com>
2018-02-05 14:01:16 -0500

    temp add 131

---
+++ Code/131/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    // Given a string s, partition s such that every substring of the partition is a palindrome.
+    //
+    // Return all possible palindrome partitioning of s.
+    //
+    // For example, given s = "aab",
+    // Return
+    //
+    // [
+    //   ["aa","b"],
+    //   ["a","a","b"]
+    // ]
+
+    // idea: DP with dfs.
+    // try DP first.
+    public List<List<String>> partition(String s) {
+        int [][] board = new int [][]
+    }
+}
