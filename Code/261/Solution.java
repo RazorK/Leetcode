1d04a67abfaf3e7f3855e4eb809621943db7704b
Razor King <aimin52qinhao@163.com>
2018-08-30 17:11:03 -0400

    261

--- Code/261/Solution.java
+++ Code/261/Solution.java
@@ -6,7 +6,7 @@ class Solution {
 
         // To check 1: use union find
         // To check 2: By following 1, we only need to check wether the number of edges equals n - 1.
-        if(n <= 1) return true;吧吧 
+        if(n <= 1) return true;
         if(edges.length != n-1) return false;
 
         int [] next = new int[n];
@@ -15,12 +15,18 @@ class Solution {
         }
 
         for(int [] edge : edges) {
-            
+            int x = getEnd(next, edge[0]);
+            int y = getEnd(next, edge[1]);
+            if(x==y) return false;
+            else {
+                next[x] = y;
+            }
         }
 
+        return true;
     }
 
-    public int getEnd(int next, int i) {
+    public int getEnd(int [] next, int i) {
         // promise that next will have no loops in it.
         while(next[i]!=i) {
             i = next[i];
